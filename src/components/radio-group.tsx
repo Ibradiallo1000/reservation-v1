b1f5d9295816aacd48976fedbb2063e07aad93c0
lbradiallo1000 <monbillet1000@gmail.com>
2025-06-11 17:42:01 +0000

    fix: remove broken radio-group component causing Netlify build failure

--- src/components/radio-group.tsx
+++
@@ -1,5 +0,0 @@
-// radio-group.tsx
-export const RadioGroup = ({ children, ...props }) => <div {...props}>{children}</div>;
-export const RadioGroupItem = ({ id, value }) => (
-  <input type="radio" id={id} name="tripType" value={value} className="accent-blue-600" />
-);
