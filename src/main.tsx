e0b10903169ff157617bef5a87846e99c549bb70
lbradiallo1000 <monbillet1000@gmail.com>
2025-07-15 16:04:32 +0000

    ✅ Correction de BrowserRouter et nettoyage du code principal

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,21 @@ import App from './App';
 import './index.css';
 import './i18n';
 
+// Nettoyage des Service Workers existants
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => registration.unregister());
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter future={
-      {
-        // @ts-ignore
-        v7_startTransition: true,
-        // @ts-ignore
-        v7_relativeSplatPath: true
-      }
-    }>
+    <BrowserRouter future={{
+      // @ts-ignore
+      v7_startTransition: true,
+      // @ts-ignore
+      v7_relativeSplatPath: true
+    }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
