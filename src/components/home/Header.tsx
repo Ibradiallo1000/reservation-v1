7826d121a0e1c466a64fa17d681f78cdce6f6185
lbradiallo1000 <monbillet1000@gmail.com>
2025-10-15 17:02:14 +0000

    ✅ Sauvegarde version stable

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -133,7 +133,7 @@ const Header: React.FC = () => {
             aria-label="Voir mes réservations"
             title="Voir mes réservations"
           >
-            <Ticket className="h-7 w-7" />
+            <Ticket className="h-6 w-6" />
             <span>Mes réservations</span>
           </button>
 
