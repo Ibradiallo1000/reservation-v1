b62b7571e4dd477c6466711c0b4f17304452c186
lbradiallo1000 <monbillet1000@gmail.com>
2026-06-04 14:23:23 +0000

    version stable prod

---
+++ dist/assets/js/courierSessionPaths-AY6nrmR6.js
@@ -0,0 +1,1 @@
+import{cD as r,cO as i}from"./vendor-DHCmXFzB.js";const c="courierSessions";function t(e,o,s){return r(e,"companies",o,"agences",s,c)}function u(e,o,s,n){return i(e,"companies",o,"agences",s,c,n)}export{t as a,u as c};
--- dist/assets/js/skeleton-B7ythYUi.js
+++
@@ -1,1 +0,0 @@
-import{dh as s}from"./vendor-BKcL7oFN.js";const d=({className:a="",...e})=>s("div",{className:`animate-pulse rounded-md bg-gray-200 ${a}`,...e});export{d as S};
